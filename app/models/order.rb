109eb4d2b7b07b8b9184dc0d9b96aafa745653d5
osadasami <osadasami@gmail.com>
2022-07-14 13:15:24 +0500

    fix filtering by date

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,8 +11,8 @@ class Order < ApplicationRecord
   scope :order_by_service, ->(direction) { order("services.name #{direction}") }
   scope :order_by_worker, ->(direction) { order("workers.name #{direction}") }
 
-  scope :filter_by_date_from, -> (date) { where("created_at >= ?", date) }
-  scope :filter_by_date_to, -> (date) { where("created_at <= ?", date) }
+  scope :filter_by_date_from, -> (date) { where("orders.created_at >= ?", date) }
+  scope :filter_by_date_to, -> (date) { where("orders.created_at <= ?", date) }
   scope :filter_by_customer_name, -> (name) { where('customer_name like ?', "%#{name}%") }
   scope :filter_by_category, -> (name) { references(order_items: [{service: :category}]).where('categories.name like ?', "%#{name}%") }
   scope :filter_by_service, -> (name) { references(order_items: :service).where('services.name like ?', "%#{name}%") }
